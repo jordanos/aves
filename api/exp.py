15dce01aaf97b4146a2fc876bda4b90e0ab87eee
jordanos <y.dereje@yahoo.com>
2022-04-07 15:15:28 +0300

    chore: removing all files to add git ignore and untrack some files

--- api/exp.py
+++
@@ -1,5 +0,0 @@
-import os
-from aves.settings import BASE_DIR
-
-path = os.path.join(BASE_DIR, "media")
-print(str(path))
--- api/payment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path('create/', CreatePurchaseApiView.as_view(), name="create-purchase"),
-    path('check/', CheckPurchaseApiView.as_view(), name="get-purchase"),
-    path('info/<str:id>', InfoPurchaseApiView.as_view(), name="info-purchase"),
-]
